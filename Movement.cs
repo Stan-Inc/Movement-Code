4d094d14c6ed26dc4f8b5d59ad25300ea8d1eeb6
Satan-Inc <36431133+Satan-Inc@users.noreply.github.com>
2018-02-14 09:42:41 -0700

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float PlayerMove;
+    private Rigidbody2D MyRigidbody2d;
+
+    private bool playerMovement;
+    private Vector2 lastMove;
+
+	// Use this for initialization
+	void Start () {
+        MyRigidbody2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerMovement = false;
+
+        if (Input.GetAxisRaw("Horizontal") > 0.1f || Input.GetAxisRaw("Horizontal") < -0.1f)
+        {
+            MyRigidbody2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * PlayerMove, MyRigidbody2d.velocity.y);
+            playerMovement = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.1f || Input.GetAxisRaw("Vertical") < -0.1f)
+        {
+            MyRigidbody2d.velocity = new Vector2( MyRigidbody2d.velocity.x, Input.GetAxisRaw("Vertical") * PlayerMove);
+            playerMovement = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
+
+        if(Input.GetAxisRaw("Horizontal") < 0.1f && Input.GetAxisRaw("Horizontal") > -0.1f)
+        {
+            MyRigidbody2d.velocity = new Vector2(0f, MyRigidbody2d.velocity.y);
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0.1f && Input.GetAxisRaw("Vertical") > -0.1f)
+        {
+            MyRigidbody2d.velocity = new Vector2(MyRigidbody2d.velocity.x, 0f);
+        }
+        }
+}
