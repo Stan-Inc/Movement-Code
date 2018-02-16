d3682d4a92d9cfcaaa106b393760c023f1c9f9af
Satan-Inc <36431133+Satan-Inc@users.noreply.github.com>
2018-02-15 23:22:53 -0500

    Add files via upload

---
+++ Teleporter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+    public Transform exit;
+    static Transform last;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (exit == last)
+            return;
+
+        TeleportToExit(other);
+            }
+    void OnTriggerExit2D()
+    {
+        if (exit == last)
+            last = null;
+    }
+    void TeleportToExit (Collider2D other)
+    {
+        last = transform;
+        other.transform.position = exit.transform.position;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
